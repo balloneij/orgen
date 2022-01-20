88c939cdb414e2f14d61f255101094c2f088415e
Isaac Ballone <isaac@ballone.dev>
2022-01-19 22:11:46 -0600

    Add heading conversion

--- src/orgen/converter.clj
+++ src/orgen/converter.clj
@@ -1,14 +1,25 @@
 (ns orgen.converter
   (:require [clojure.string :as str]
-            [clojure.walk :refer [prewalk]]))
+            [clojure.walk :refer [postwalk]]))
 
 (defn child [parent i]
   (nth parent i nil))
 
-(defn tag= [node value]
+(defn tag= [node tag]
   (if (sequential? node)
-    (= (first node) value)
+    (= (first node) tag)
     nil))
+
+(defn node-value [node]
+  (if (map? (second node))
+    (nthrest node 2)
+    (rest node)))
+
+(defn node-attrs [node]
+  (let [attrs (second node)]
+    (if (map? attrs)
+      attrs
+      {})))
 
 (defn right-text-siblings [parent i]
   (loop [i (inc i)
@@ -19,7 +30,7 @@
         siblings))))
 
 (defn text->paragraph [parent i node]
-  (if (tag= node :text)
+  (if (and (tag= node :text) (not (tag= parent :heading)))
     (let [left-sibling (child parent (dec i))]
       (if-not (tag= left-sibling :text)
         [:p (str/join " "
@@ -29,10 +40,17 @@
         :ignore-tag))
     nil))
 
+(defn heading->hx [node]
+  (if (tag= node :heading)
+    (let [{:keys [level]} (node-attrs node)]
+      (vec (concat [(keyword (str "h" level))] (node-value node))))
+    nil))
+
 (defn pass-1 [node]
   (if (and (sequential? node) (not (map-entry? node)))
     (reduce-kv (fn [m i v]
                  (let [result (or (text->paragraph node i v)
+                                  (heading->hx v)
                                   v)]
                    (if (= result :ignore-tag)
                      m
@@ -42,4 +60,4 @@
     node))
 
 (defn converter [document]
-  (prewalk pass-1 document))
+  (postwalk pass-1 document))
